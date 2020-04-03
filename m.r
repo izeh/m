fc78b9e8764439c6dfd2c89ee425cdc9534aa6af
izeh <31554814+izeh@users.noreply.github.com>
2020-04-02 22:41:39 -0500

    Update m.r

--- m.r
+++ m.r
@@ -4737,7 +4737,7 @@ is.unique <- function(X, which){
                                                                           
 #===============================================================================================================================
 
-                                   
+                                 
 interrate <- function(..., nsim = 1e3, level = .95, useNA = "ifany", na.rm = FALSE, digits = 3, common = FALSE, all = TRUE, drop = NULL, plot = TRUE, lwd = 5, lend = 1, show.sa = TRUE, group.level = NULL, study.level = NULL, file.name = NULL, reset = TRUE, rev.page = FALSE, cex.sa = .9)
 {
   
@@ -4753,13 +4753,11 @@ interrate <- function(..., nsim = 1e3, level = .95, useNA = "ifany", na.rm = FAL
   
   r <- full.clean(r, ar, all)
   
-  r <- lapply(r, function(i) setNames(i, trimws(names(i))))
+  r <- lapply(r, trim)
   
   check <- all(sapply(r, function(i) "study.name" %in% names(i)))
   
   if(!check) stop("Add a new column named 'study.name' to the coding sheet(s).", call. = FALSE)
-  
-  r <- lapply(r, function(i) {i$study.name <- trimws(i$study.name); i})
   
   r <- lapply(r, function(x) do.call(rbind, c(split(x, x$study.name), make.row.names = FALSE)))
   
